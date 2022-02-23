3fd69d77845af450ab34b8cb7f9b1c9a0ddc720a
Andrelise Nunes Lemos Pinheiro <andrelise.pinheiro@compasso.com.br>
2022-02-23 14:56:22 -0300

    novos testes da multiplicação

--- cypress/integration/meu-teste.spec.js
+++ cypress/integration/meu-teste.spec.js
@@ -1,24 +1,40 @@
 /// <reference types="cypress" />
 
 context('Actions', () => {
-    beforeEach(() => {
-      cy.visit('../../index.html')
-    })
+  beforeEach(() => {
+    cy.visit('../../index.html')
+  })
 
-    it('Fazer a soma com resultado certo', () => {
-        cy.get('#num7').click()
-        cy.get('#soma').click()
-        cy.get('#num5').click()
-        cy.get('#resultado').click()
-        cy.get('input#tela').should('contain.value',12)
-      })
+  it('Fazer a soma com resultado certo', () => {
+    cy.get('#num7').click()
+    cy.get('#soma').click()
+    cy.get('#num5').click()
+    cy.get('#resultado').click()
+    cy.get('input#tela').should('contain.value',12)
+  })
 
-      it('Fazer a soma com resultado errado', () => {
-        cy.get('#num8').click()
-        cy.get('#soma').click()
-        cy.get('#num5').click()
-        cy.get('#resultado').click()
-        cy.get('input#tela').should('contain.value',12)
-      })
+  it('Fazer a soma com resultado errado', () => {
+    cy.get('#num8').click()
+    cy.get('#soma').click()
+    cy.get('#num5').click()
+    cy.get('#resultado').click()
+    cy.get('input#tela').should('contain.value',12)
+  })
 
-    })
+  it('Fazer a multiplicação certa', () => {
+    cy.get('#num7').click()
+    cy.get('#multiplicacao').click()
+    cy.get('#num5').click()
+    cy.get('#resultado').click()
+    cy.get('input#tela').should('contain.value', 35)
+  })
+
+  it('Fazer a multiplicação certa', () => {
+    cy.get('#num7').click()
+    cy.get('#multiplicacao').click()
+    cy.get('#num7').click()
+    cy.get('#resultado').click()
+    cy.get('input#tela').should('contain.value', 49)
+  })
+
+})
