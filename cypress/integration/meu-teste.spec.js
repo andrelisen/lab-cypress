a37de4f6dd0170e4eb72f54bcdd706036d6b8572
Andrelise Nunes Lemos Pinheiro <andrelise.pinheiro@compasso.com.br>
2022-02-23 14:05:10 -0300

    arquivos de teste e a calculadora

---
+++ cypress/integration/meu-teste.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+    beforeEach(() => {
+      cy.visit('../../index.html')
+    })
+
+    it('Fazer a soma com resultado certo', () => {
+        cy.get('#num7').click()
+        cy.get('#soma').click()
+        cy.get('#num5').click()
+        cy.get('#resultado').click()
+        cy.get('input#tela').should('contain.value',12)
+      })
+
+      it('Fazer a soma com resultado errado', () => {
+        cy.get('#num8').click()
+        cy.get('#soma').click()
+        cy.get('#num5').click()
+        cy.get('#resultado').click()
+        cy.get('input#tela').should('contain.value',12)
+      })
+
+    })
